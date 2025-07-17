2d3c41482b02333877c1ee63dc6988b322663636
khloy <pronnierchloe@outlook.com>
2025-07-17 16:24:21 +0400

    exercice fait

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+nom = "P"
+age = 28
+taille = 1.59
+est_etudiant = True
+
+print("Nom : " +nom)
+print("Age : " + str(age))
+print("Taille : " + str(taille))
+print("Est étudiant : "+ str(est_etudiant))
+
+print("Type nom : " + str(type[nom]))
+print("Type age : " + str(type[age]))
+print("Type taille : " + str(type[taille]))
+print("Type est_etudiant : " + str(type[est_etudiant]))

d86eb2032f87647cf412c25aa1c7ea92845ddc42
khloy <pronnierchloe@outlook.com>
2025-07-18 16:59:48 +0400

    exerice fait

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,1 +1,13 @@
-# Ecrivez votre code ici !
+import operations
+
+nombre1 = 3
+nombre2 = 5
+resultat_addition = operations.addition(nombre1, nombre2)
+
+nombre3 = 8
+nombre4 = 2
+resultat_multiplication = operations.multiplication(nombre3, nombre4)
+
+print(f"{nombre1} + {nombre2} = {resultat_addition}")
+print(f"{nombre3} * {nombre4} = {resultat_multiplication}")
+
